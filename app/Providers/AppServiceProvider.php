d2a9a975fed899d3658e37a2b00b46a35e555070
Chris Southam <chris.southam@wearedomo.com>
2025-04-23 18:43:36 +0100

    Automatically eager load relationships

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::automaticallyEagerLoadRelationships();
     }
 }
